6e0b6f9b29577a6dcb3c6cccf52ee2ddcee6e32b
Adrian Kirsten <adrian.kirsten@traaidmark.com>
2013-12-18 12:24:32 +0200

    Optimizing file structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,12 @@ module.exports = function(grunt) {
     // 1. All configuration goes here 
     grunt.initConfig({
 
-		// 1.1. Configure grunt-contrib-concat
+		// 1.1. Concatenate Javascript assets
         concat: {   
             dist: {
                 src: [
-                    'assets/js/scripts/*.js'  // This specific file
+                    'assets/js/libraries/*.js',
+                    'assets/js/scripts/*.js'
                 ],
                 dest: 'assets/js/production.js',
             }
@@ -15,7 +16,12 @@ module.exports = function(grunt) {
         // 1.1. End
         
         // 1.2. Uglify Javascript
-        
+        uglify: {
+            build: {
+                src: 'assets/js/production.js',
+                dest: 'assets/js/production.min.js'
+            }
+        },
         // 1.2. End
         
         // 1.3. Configure grunt-contrib-concat
@@ -35,6 +41,7 @@ module.exports = function(grunt) {
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
